e80fdbba59d7ee934345d3c06a4e208d793133cb
Gabriel Dorfsman-Hopkins <wackoliberal@gmail.com>
2026-07-08 17:07:56 -0400

    Created an IMSO page

---
+++ imso.js
@@ -0,0 +1,31 @@
+fetch("data/imso.json")
+    .then(response => response.json())
+    .then(talks => {
+
+        talks.sort((a,b) =>
+            new Date(b.date) - new Date(a.date)
+        );
+
+        const gallery = document.getElementById("seminar-gallery");
+
+        talks.forEach(talk => {
+
+            const card = document.createElement("a");
+            card.href = talk.url;
+            card.target = "_blank";
+            card.className = "poster-card";
+
+            card.innerHTML = `
+                <img src="${talk.poster}" alt="${talk.title}">
+                <div class="poster-overlay">
+                    <strong>${talk.speaker}</strong><br>
+                    ${talk.title}<br>
+                    ${talk.date}
+                </div>
+            `;
+
+            gallery.appendChild(card);
+
+        });
+
+    });
